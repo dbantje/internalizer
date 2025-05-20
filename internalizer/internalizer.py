1210a3ee5576eba120ad30e99ada4eb14c1781e2
dbantje <david.bantje@pik-potsdam.de>
2025-05-20 16:13:51 +0200

    Imports optimization

--- internalizer/internalizer.py
+++ internalizer/internalizer.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Dict
+from typing import List, Optional
 from pathlib import Path
 import os
 
--- internalizer/regionalization.py
+++ internalizer/regionalization.py
@@ -3,7 +3,6 @@ import subprocess
 from pathlib import Path
 import os
 import numpy as np
-import xarray as xr
 import uuid
 
 from premise.geomap import Geomap
